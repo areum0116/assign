49d36c392ef8a2fe2fb9f3eb6b4cabc7acd7e2f8
areum <areum0116@naver.com>
2025-04-04 18:22:25 +0900

    [feat] addCorporationNumber

--- src/main/java/com/example/assignment/domain/company/service/CompanyService.java
+++ src/main/java/com/example/assignment/domain/company/service/CompanyService.java
@@ -3,12 +3,17 @@ package com.example.assignment.domain.company.service;
 import com.example.assignment.domain.company.dto.request.CompanySaveRequest;
 import com.example.assignment.domain.company.dto.response.CompanySaveResponse;
 import com.example.assignment.domain.company.repository.CompanyRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,7 +25,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -33,6 +38,13 @@ public class CompanyService {
     private static final String DOWNLOAD_PATH = "C:/Users/arkim/Workspace/assignment/src/main/resources/static/download/";
     private String fileName;
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Value("${api.fair-trade-commission.base-url}")
+    private String fairTradeUrl;
+    @Value("${api.fair-trade-commission.key}")
+    private String fairTradeKey;
+
     private final CompanyRepository companyRepository;
 
     // 파일 다운로드
@@ -82,6 +94,81 @@ public class CompanyService {
         }
     }
 
+    // 법인등록번호 추가
+    private void addCorporationNumber(String inputPath, String outputPath) throws Exception {
+        Set<String> brnoSet = new HashSet<>();
+        List<String[]> csvRows = new ArrayList<>();
+
+        try(
+                BufferedReader reader = Files.newBufferedReader(Paths.get(inputPath));
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputPath))
+        ) {
+            String headerLine = reader.readLine();
+            if(headerLine == null || headerLine.isEmpty()) throw new RuntimeException("파일이 없거나 헤더를 찾을 수 없습니다.");
+
+            String[] header = headerLine.split(",");
+            int brnoIdx = Arrays.asList(header).indexOf("사업자등록번호");
+            if(brnoIdx == -1) throw new RuntimeException("해당 컬럼을 찾을 수 없습니다.");
+
+            String line;
+            while((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                if (fields.length > brnoIdx) {
+                    brnoSet.add(fields[brnoIdx].trim());
+                }
+                csvRows.add(fields);
+            }
+
+            Map<String, String> brnoToCrno = fetchAllCrno(brnoSet);
+
+            for (String[] row : csvRows) {
+                if (row.length == 0) continue;
+                String brno = row[brnoIdx].trim();
+                String crno = brnoToCrno.getOrDefault(brno, "");
+                writer.write(String.join(",", ArrayUtils.add(row, crno)));
+                writer.newLine();
+            }
+        }
+    }
+
+    // 공정거래위원회 통신판매사업자 등록상세 api 호출 통해 법인등록번호 가져오기
+    private Map<String, String> fetchAllCrno(Set<String> brnos) throws Exception {
+
+        Map<String, String> resultMap = new HashMap<>();
+        int pageNo = 1;
+        int numOfRows = 1000;
+        int totalCount = Integer.MAX_VALUE;
+
+        while ((pageNo - 1) * numOfRows < totalCount) {
+            String url = UriComponentsBuilder.fromUriString(fairTradeUrl)
+                    .queryParam("serviceKey", fairTradeKey)
+                    .queryParam("pageNo", pageNo)
+                    .queryParam("numOfRows", numOfRows)
+                    .build()
+                    .toUriString();     // 인코딩 x url
+
+            try {
+                String response = restTemplate.getForObject(url, String.class);
+                JsonNode root = objectMapper.readTree(response);    // string -> json
+                totalCount = root.path("response").path("body").path("totalCount").asInt();
+
+                JsonNode items = root.path("response").path("body").path("items");
+                for (JsonNode item : items) {
+                    String brno = item.path("brno").asText();
+                    String crno = item.path("crno").asText();
+                    if (brnos.contains(brno)) {
+                        resultMap.put(brno, crno);
+                    }
+                }
+
+                pageNo++;
+            } catch (Exception e) {
+                throw new RuntimeException("API 응답 처리 중 오류", e);
+            }
+        }
+        return resultMap;
+    }
+
     public CompanySaveResponse saveCompany(CompanySaveRequest requestDto) throws Exception {
         // 파일명 url 인코딩
         String encodedFileName = URLEncoder.encode("통신판매사업자_" + requestDto.getCity() + "_" + requestDto.getDistrict() + ".csv", StandardCharsets.UTF_8);
@@ -110,7 +197,12 @@ public class CompanyService {
             log.info(message);
 
             // 법인 필터링
-            filterCompanyByCorporation(DOWNLOAD_PATH + fileName, DOWNLOAD_PATH + "filtered_" + fileName);
+            String filteredFilePath = DOWNLOAD_PATH + "filtered_" + fileName;
+            filterCompanyByCorporation(DOWNLOAD_PATH + fileName, filteredFilePath);
+
+            // 파일에 법인등록번호 추가
+            String addCorporationNumberFilePath = DOWNLOAD_PATH + "filtered_with_crno_" + fileName;
+            addCorporationNumber(filteredFilePath, addCorporationNumberFilePath);
 
             // 결과 반환
             return new CompanySaveResponse(response.getStatusCode().value(), message);
